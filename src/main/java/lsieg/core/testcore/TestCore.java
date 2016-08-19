9a4c7bbb07fd2ebb3b1e3e07a7341dcdbf7ace55
lsieg <leonard.s.100@gmail.com>
2016-08-19 07:50:25 -0400

    Add Core project

---
+++ src/main/java/lsieg/core/testcore/TestCore.java
@@ -0,0 +1,16 @@
+package lsieg.core.testcore;
+
+public class TestCore {
+	
+	public String testString;
+	
+	public TestCore()
+	{
+		testString = "Hello World!";
+	}
+	
+	public String getTestString()
+	{
+		return testString;
+	}
+}
